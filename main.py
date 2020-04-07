fef973ea68fd5e4ff8a472b5750a56e5514e472c
OmarKhathlan <59093770+omarkh1122@users.noreply.github.com>
2020-04-07 23:54:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,107 @@ from DFS import *
 from BFS import *
 from IDFS import *
 from GreadySearch import *
-import time
 from GenerateState import GenerateState
 from Node import Node
 import numpy as np
+import time
+
+a = AStarAlgo()
+d = DFS()
+b = BFS()
+i = IDFS()
+g = GreadySearch()
+
+def batch(state, depth = 10):
+    
+    # A* Search.
+    start = time.time()
+    sol = a.a_star_algorithm(state)
+    print(f'Time: {time.time() - start}')
+    print(sol)
+    
+    time.sleep(5)
+    # Gready Search.
+    start = time.time()
+    g.gready_search_algorithm(state)
+    print(f'Time: {time.time() - start}')
+    print(sol)
+    
+    time.sleep(5)
+    # Breadth First Search.
+    start = time.time()
+    sol = b.BFS(state)
+    print(f'Time: {time.time() - start}')
+    print(sol)
+    
+    time.sleep(5)
+    # Iterative Deapening Search.
+    start = time.time()
+    sol = i.IDFS(state)
+    print(f'Time: {time.time() - start}')
+    print(sol)
+    
+    time.sleep(5)
+    # Depth Limited First Search.
+    start = time.time()
+    sol = i.DLFS(state, depth)
+    print(f'Time: {time.time() - start}')
+    print(sol)
+    
+    time.sleep(5)
+    # Depth First Search.
+    start = time.time()
+    sol = d.DFS(state)
+    print(f'Time: {time.time() - start}')
+    print(sol)
+    
+
+def randomState():
+    return GenerateState()
+
+
+# state = [
+#     1,2,3,
+#     4,5,6,
+#     0,7,8
+# ]
+
+state = [
+    4,7,2,
+    5,0,3,
+    6,8,1
+]
+
+batch(state)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -------------------------- Tests --------------------------------- #
 
 
 # state = [1,2,3,
@@ -22,30 +116,13 @@ import numpy as np
 #     print(i.state)
 
 
-state = GenerateState()
-
-a = AStarAlgo()
-# d = DFS()
-# b = BFS()
-# i = IDFS()
-# g = GreadySearch()
+# state = GenerateState()
 
 # print(tmp)
 # state=[
 #     1,2,3,4,5,0,6,7,8
 # ]
 
-# state=[
-#     4,7,2,
-#     5,0,3,
-#     6,8,1
-# ]
-
-# # state=[
-# #     1,2,3,
-# #     4,5,6,
-# #     0,7,8
-# # ]
 
 
 # start = time.time()
@@ -58,44 +135,44 @@ a = AStarAlgo()
 # sol = d.DFS(state)
 # print(f'Time: {time.time() - start}')
 # print(sol)
-# time.sleep(10.0)
+# 
 
 # start = time.time()
 # sol = b.BFS(state)
 # print(f'Time: {time.time() - start}')
 # print(sol)
-# time.sleep(10.0)
+# 
 
 # start = time.time()
 # sol = i.IDFS(state)
 # print(f'Time: {time.time() - start}')
 # print(sol)
-# time.sleep(10.0)
+# 
 
 # start = time.time()
 # sol = i.DLFS(state, 10)
 # print(f'Time: {time.time() - start}')
 # print(sol)
-# time.sleep(10.0)
+# 
 
-times = []
+# times = []
 # s=[
 #     4,7,2,
 #     5,0,3,
 #     6,8,1
 # ]
 
-s = [ 
-      8,6,7,
-      2,5,4,
-      3,0,1
-    ]
+# s = [ 
+#       8,6,7,
+#       2,5,4,
+#       3,0,1
+#     ]
 
-for i in range(3):
-    start = time.time()
-    sol = a.a_star_algorithm(s)
-    times.append(time.time() - start)
-    print(sol)
+# for i in range(3):
+#     start = time.time()
+#     sol = a.a_star_algorithm(s)
+#     times.append(time.time() - start)
+#     print(sol)
 
-avg = np.average(times)
-print(avg)
+# avg = np.average(times)
+# print(avg)
